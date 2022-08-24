795351e3155af650c32d764aafb695bf8795fc80
fanyi21 <111823574+fanyi21@users.noreply.github.com>
2022-08-24 10:31:24 +0800

    Delete function directory

--- function/fit_obj.m
+++
@@ -1,9 +0,0 @@
-function V = fit_obj(X, CR, flag)
-    cp = valid_compactness(X, CR); 
-    Dev=Devation(X, CR);      
-    if flag == 1
-        V = [1000*cp, Dev];
-    else
-        V = [-1000*cp, -Dev];
-    end
-end
--- function/initial_fitness_2.m
+++
@@ -1,5 +0,0 @@
-function [obj_fitness] = initial_fitness_2(group_data, label, SI_flag)
-
-    obj_fitness = fit_obj(group_data, label, SI_flag);
-
-end
