1e190847ad25bef698141dd52a84ec5e82aefefa
fanyi21 <111823574+fanyi21@users.noreply.github.com>
2023-03-13 20:49:44 +0800

    repos = "http://cran.us.r-project.org"

--- DEPF/HierarchicalAutoencoder/runHA.R
+++ DEPF/HierarchicalAutoencoder/runHA.R
@@ -2,7 +2,7 @@ pkg <- c("R.matlab", "SingleCellExperiment", "scDHA")
 ipak <- function(pkg){
   new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
   if (length(new.pkg)) 
-    install.packages(new.pkg, dependencies = TRUE)
+    install.packages(new.pkg, dependencies = TRUE, repos = "http://cran.us.r-project.org")
   sapply(pkg, require, character.only = TRUE)
 }
 ipak(pkg)
